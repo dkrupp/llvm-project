14a8ecb42d1eb1449452cfcfc0fd365040150dbb
Daniel Krupp <daniel.krupp@ericsson.com>
2024-10-03 18:50:38 +0200

    Add InlineTaintOnly feature

--- clang/include/clang/StaticAnalyzer/Core/AnalyzerOptions.h
+++ clang/include/clang/StaticAnalyzer/Core/AnalyzerOptions.h
@@ -259,6 +259,7 @@ public:
   unsigned AnalyzeAll : 1;
   unsigned AnalyzerDisplayProgress : 1;
   unsigned AnalyzerFocusedTaint : 1;
+  unsigned AnalyzerInlineTaintOnly : 1;
   unsigned AnalyzerNoteAnalysisEntryPoints : 1;
 
   unsigned TrimGraph : 1;
@@ -324,6 +325,7 @@ public:
         ShouldEmitErrorsOnInvalidConfigValue(false), AnalyzeAll(false),
         AnalyzerDisplayProgress(false), AnalyzerNoteAnalysisEntryPoints(false),
         AnalyzerFocusedTaint(false),
+        AnalyzerInlineTaintOnly(false),
         TrimGraph(false), visualizeExplodedGraphWithGraphViz(false),
         UnoptimizedCFG(false), PrintStats(false), NoRetryExhausted(false),
         AnalyzerWerror(false) {}
